e2d6e0f0a91d5d7cb1cd28af24e6a44393a9ed38
josevi97 <josevi00@hotmail.com>
2024-07-06 15:22:44 +0200

    feat: adds frontend project

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      "jsx": true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
+  },
+}
---
+++ frontend/src/Application.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
+
+const Application = () => {
+  return (
+    <BrowserRouter>
+      Esto es una prueba de codigo para ver que tan facil se formatea
+    </BrowserRouter>
+  );
+};
+
+export default Application;
