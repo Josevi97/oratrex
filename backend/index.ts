804c0ee268f67975e9b924f410a67ba5e76fd1ce
josevi97 <josevi00@hotmail.com>
2024-07-07 19:39:23 +0200

    feat: commented protecting csrf

--- backend/index.ts
+++ backend/index.ts
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// app.use(csrfProtection);
 app.use('/api', router);
 
 app.listen(SERVER_PORT, () => {
